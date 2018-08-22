b569d0df725287f3c8eddc11dde109ff276c3f71
Rajasegar <rajasegarc@freshdesk.com>
2018-08-22 20:24:21 +0530

    Tests and coverage stuff

--- tests/integration/components/accordion-panel-test.js
+++ tests/integration/components/accordion-panel-test.js
@@ -1,22 +1,95 @@
-import { module, test } from 'qunit';
+import { module, test, skip } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
+import { find, render } from '@ember/test-helpers';
 import hbs from 'htmlbars-inline-precompile';
 
 module('Integration | Component | accordion-panel', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.set('myAction', function(val) { ... });
-
+  hooks.beforeEach(async function() {
     this.set('selectCallback', function(e) {
       this.set('active', e);
     });
 
-    await this.render(hbs`{{accordion-panel title='Hello World' select=(action selectCallback)}}`);
+    await render(hbs`{{#accordion-panel title='Hello World' select=(action selectCallback)}}Hello World{{/accordion-panel}}`);
 
-    // assert.equal(this.element.textContent.trim(), '');
-    assert.equal(1,1);
+  });
 
+  test('should have a dt element', function(assert) {
+    assert.ok(find('dt'));
+  });
+
+  test('should have a dt element with role=heading', function(assert) {
+    assert.ok(find('dt[role="heading"]'));
+  });
+
+  test('should have a dt element with aria-level=3', function(assert) {
+    assert.ok(find('dt[aria-level="3"]'));
+  });
+
+
+  test('should have a button element', function(assert) {
+    assert.ok(find('button'));
+  });
+
+  test('should have a button element with class=accordion-trigger', function(assert) {
+    assert.ok(find('button[class="accordion-trigger"]'));
+  });
+
+  test('should have a button element with type=button', function(assert) {
+    assert.ok(find('button[type="button"]'));
+  });
+
+  test('should have a button element with aria-expanded=false', function(assert) {
+    assert.ok(find('button[aria-expanded="false"]'));
+  });
+
+  skip('should have a button element with aria-expanded=true', function(assert) {
+    this.set('active', 'Hello World');
+    assert.ok(find('button[aria-expanded="true"]'));
+  });
+
+
+  test('should have a button element with aria-controls=sectionId', function(assert) {
+    assert.ok(find('button[aria-controls="sect-HelloWorld"]'));
+  });
+
+  test('should have a button element with id=accordionId', function(assert) {
+    assert.ok(find('button[id="accordion-HelloWorld"]'));
+  });
+
+  test('should have a span element with class=accordion-title', function(assert) {
+    assert.ok(find('span[class="accordion-title"]'));
+  });
+
+  test('should have a span element with title', function(assert) {
+    let titleEl = find('span[class="accordion-title"]');
+    assert.equal(titleEl.textContent.trim(), 'Hello World');
+  });
+
+
+  test('should have a span element with class=accordion-icon', function(assert) {
+    assert.ok(find('span[class="accordion-icon"]'));
+  });
+
+  test('should have a dd element with id=sectId', function(assert) {
+    assert.ok(find('dd[id="sect-HelloWorld"]'));
+  });
+
+  test('should have a dd element with role=region', function(assert) {
+    assert.ok(find('dd[role="region"]'));
+  });
+
+  test('should have a dd element with aria-labelledby=accordionId', function(assert) {
+    assert.ok(find('dd[aria-labelledby="accordion-HelloWorld"]'));
+  });
+
+  test('should have a dd element with class=accordion-panel', function(assert) {
+    assert.ok(find('dd[class="accordion-panel"]'));
+  });
+
+  test('should have a dd element with yielded content', function(assert) {
+    let panelEl = find('dd[class="accordion-panel"]');
+    assert.equal(panelEl.textContent.trim(), "Hello World");
   });
 });
--- tests/integration/components/aria-accordion-test.js
+++ tests/integration/components/aria-accordion-test.js
@@ -1,21 +1,122 @@
-import { module, test } from 'qunit';
+import { module, test, skip } from 'qunit';
 import { setupRenderingTest } from 'ember-qunit';
 import hbs from 'htmlbars-inline-precompile';
+import { find, findAll, click, render } from '@ember/test-helpers';
+
 
 module('aria-accordion', 'Integration | Component | aria accordion', function(hooks) {
   setupRenderingTest(hooks);
 
-  test('it renders', async function(assert) {
+  hooks.beforeEach(async function() {
+    this.set('active', 'Home');
 
-    // Set any properties with this.set('myProperty', 'value');
-    // Handle any actions with this.on('myAction', function(val) { ... });
+    await render(hbs`
+    {{#aria-accordion active=active  as |a|}}
+    {{#a.panel title="Personal Information" }}
+      <p> Personal Information</p>
+    {{/a.panel}}
+    {{#a.panel title="Home" }}
+      <p> Home </p>
+    {{/a.panel}}
+    {{#a.panel title="Settings" }}
+      <p> Settings </p>
+    {{/a.panel}}
+    {{/aria-accordion}}
+    `);
+  });
 
-    await this.render(hbs`{{aria-accordion}}`);
+  test('should have a dl element with class=accordion', function(assert) {
+    assert.ok(find('dl.accordion'));
+  });
 
-    // assert.equal(this.$().text().trim(), '');
-    assert.equal(1,1);
+  test('should have a dl element with role=presentation', function(assert) {
+    assert.ok(find('dl[role="presentation"]'));
+  });
 
+  test('should have 3 dd elements', function(assert) {
+    assert.equal(findAll('dd').length, 3);
+  });
+
+  test('should have 3 dt elements', function(assert) {
+    assert.equal(findAll('dt').length, 3);
+  });
+
+  test('should have only one active panel', function(assert) {
+    assert.equal(findAll('button[aria-expanded="true"]').length, 1);
+    assert.equal(this.get('active'), 'Home');
+  });
+
+  test('should activate the appropriate panel', async function(assert) {
+    await click('button#accordion-Settings');
+    assert.equal(this.get('active'), 'Settings');
   });
 
 });
 
+module('aria-accordion with toggle=true', 'Integration | Component | aria accordion', function(hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(async function() {
+    this.set('active', 'Home');
+
+    await render(hbs`
+    {{#aria-accordion active=active  toggle=true as |a|}}
+    {{#a.panel title="Personal Information" }}
+      <p> Personal Information</p>
+    {{/a.panel}}
+    {{#a.panel title="Home" }}
+      <p> Home </p>
+    {{/a.panel}}
+    {{#a.panel title="Settings" }}
+      <p> Settings </p>
+    {{/a.panel}}
+    {{/aria-accordion}}
+    `);
+  });
+
+  test('should toggle the appropriate panel', async function(assert) {
+    assert.equal(findAll('button[aria-expanded="true"]').length, 1);
+    await click('button#accordion-Home');
+    assert.equal(findAll('button[aria-expanded="true"]').length, 0);
+  });
+
+
+});
+
+module('aria-accordion with multiple=true', 'Integration | Component | aria accordion', function(hooks) {
+  setupRenderingTest(hooks);
+
+  hooks.beforeEach(async function() {
+    this.set('active', 'Home');
+
+    // TODO: Also pass toggle=true and check
+    await render(hbs`
+    {{#aria-accordion active=active  multiple=true as |a|}}
+    {{#a.panel title="Personal Information" }}
+      <p> Personal Information</p>
+    {{/a.panel}}
+    {{#a.panel title="Home" }}
+      <p> Home </p>
+    {{/a.panel}}
+    {{#a.panel title="Settings" }}
+      <p> Settings </p>
+    {{/a.panel}}
+    {{/aria-accordion}}
+    `);
+  });
+
+  test('should activate multiple panels', async function(assert) {
+    assert.equal(findAll('button[aria-expanded="true"]').length, 1);
+    await click('button#accordion-Settings');
+    assert.equal(findAll('button[aria-expanded="true"]').length, 2);
+  });
+
+  // TODO: Need to check these cases
+  skip('should toggle/activate multiple panels', async function(assert) {
+    assert.equal(findAll('button[aria-expanded="true"]').length, 1);
+    await click('button#accordion-Settings');
+    assert.equal(findAll('button[aria-expanded="true"]').length, 2);
+    await click('button#accordion-Settings');
+    assert.equal(findAll('button[aria-expanded="true"]').length, 1);
+  });
+});
